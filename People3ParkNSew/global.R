9403c0c12cada0cd8ab41de90bbc7d70e769be83
Matt Parker <mattparker@matts-mbp.attlocal.net>
2020-12-12 20:30:51 -0600

    Created fileInput and fileOutputs for user data

--- People3ParkNSew/global.R
+++ People3ParkNSew/global.R
@@ -10,6 +10,14 @@ age_df <- data.frame(census_df_list[1])
 race_df <- data.frame(census_df_list[2])
 edu_df <- data.frame(census_df_list[3])
 
-View(age_df)
-View(race_df)
-View(edu_df)
+#View(age_df)
+#View(race_df)
+#View(edu_df)
+
+read_excel_allsheets <- function(filename, tibble = FALSE) {
+  sheets <- readxl::excel_sheets(filename)
+  x <- lapply(sheets, function(X) readxl::read_excel(filename, sheet = X))
+  if(!tibble) x <- lapply(x, as.data.frame)
+  names(x) <- sheets
+  x
+}
