c21ba02401fc30d3da4f7fbd459b786fb83ee28c
Savannah Sew-Hee <savrosealbright@gmail.com>
2020-12-15 10:48:49 -0600

    pivot table longer df in global, added plots to age df, need to make them side by side bar plots on same axis, but running into problems

--- People3ParkNSew/server.R
+++ People3ParkNSew/server.R
@@ -7,7 +7,6 @@ shinyServer(function(input, output, session) {
     })
     
     
-    
     # Matt's Output for his Page
     #output$user_data_output <- renderTable({
         # input$file1 will be NULL initially. After the user selects
@@ -27,19 +26,132 @@ shinyServer(function(input, output, session) {
         #user_race
         #user_edu
     
-    #Savannah testing trying to make 3 outputs 1 for each data frame    
+    output$age_total_plot <- renderPlot({
+        ggplot(data = Datalong_age[1:6,]) + 
+            geom_col(aes(x = age_group, y = value)) +
+            labs(title = "Number of People per Age Group in Nashville", 
+                 x = "Age Groups", y = "Total") +
+            scale_x_discrete(labels=c("age_under_20_years" = "Under 20", "age_20_29_years" = "20 to 29",
+                                      "age_30_39_years" = "30 to 39", "age_40_49_years" = "40 to 49",
+                                      "age_50_59_years" = "50 to 59", "age_60_years_and_over" = "Over 60"))
+    })
+    
+    output$age_male_plot <- renderPlot({
+        ggplot(data = Datalong_age[7:12,]) + 
+            geom_col(aes(x = age_group, y = value)) +
+            labs(title = "Number of Males per Age Group in Nashville", 
+                 x = "Age Groups", y = "Total") +
+            scale_x_discrete(labels=c("age_under_20_years" = "Under 20", "age_20_29_years" = "20 to 29",
+                                      "age_30_39_years" = "30 to 39", "age_40_49_years" = "40 to 49",
+                                      "age_50_59_years" = "50 to 59", "age_60_years_and_over" = "Over 60"))
+    })
+    
+    output$age_female_plot <- renderPlot({
+        ggplot(data = Datalong_age[13:18,]) + 
+            geom_col(aes(x = age_group, y = value)) +
+            labs(title = "Number of Females per Age Group in Nashville", 
+                 x = "Age Groups", y = "Total") +
+            scale_x_discrete(labels=c("age_under_20_years" = "Under 20", "age_20_29_years" = "20 to 29",
+                                      "age_30_39_years" = "30 to 39", "age_40_49_years" = "40 to 49",
+                                      "age_50_59_years" = "50 to 59", "age_60_years_and_over" = "Over 60"))
+    })
+    
+    #Making side by side bar chart comparing age 
+    output$user_age_total<- renderPlot({
+        #making data frame
+        user_data1 <- read_excel_allsheets(input$user_file$datapath)[1]
+        user_data1 <- data.frame(user_data1)
+        
+        #pivot longer
+        user_data1 <- pivot_longer(user_data1, cols = Age.Under.20.years: Age.60.years.and.over, names_to = "age_group")
+        
+        #setting factors so bar chart in right order
+        user_data1$age_group <- factor(user_data1$age_group,levels = c("Age.Under.20.years", 
+                                                                        "Age.20.to.29.years", 
+                                                                        "Age.30.to.39.years", 
+                                                                        "Age.40.to.49.years",
+                                                                        "Age.50.to.59.years",
+                                                                        "Age.60.years.and.over"))
+        #plotting bar chart
+        ggplot(data = user_data1[1:6,]) + 
+            geom_col(aes(x = age_group, y = value)) +
+            labs(title = "Number of People per Age Group for Your Company", 
+                 x = "Age Groups", y = "Total") +
+            scale_x_discrete(labels=c("Age.Under.20.years" = "Under 20", "Age.20.to.29.years" = "20 to 29",
+                                      "Age.30.to.39.years" = "30 to 39", "Age.40.to.49.years" = "40 to 49",
+                                      "Age.50.to.59.years" = "50 to 59", "Age.60.years.and.over" = "Over 60"))
+    })
+    
+    output$user_age_male<- renderPlot({
+        #making data frame
+        user_data1 <- read_excel_allsheets(input$user_file$datapath)[1]
+        user_data1 <- data.frame(user_data1)
+        
+        #pivot longer
+        user_data1 <- pivot_longer(user_data1, cols = Age.Under.20.years: Age.60.years.and.over, names_to = "age_group")
+        
+        #setting factors so bar chart in right order
+        user_data1$age_group <- factor(user_data1$age_group,levels = c("Age.Under.20.years", 
+                                                                       "Age.20.to.29.years", 
+                                                                       "Age.30.to.39.years", 
+                                                                       "Age.40.to.49.years",
+                                                                       "Age.50.to.59.years",
+                                                                       "Age.60.years.and.over"))
+        #plotting bar chart
+        ggplot(data = user_data1[7:13,]) + 
+            geom_col(aes(x = age_group, y = value)) +
+            labs(title = "Number of Males per Age Group for Your Company", 
+                 x = "Age Groups", y = "Total") +
+            scale_x_discrete(labels=c("Age.Under.20.years" = "Under 20", "Age.20.to.29.years" = "20 to 29",
+                                      "Age.30.to.39.years" = "30 to 39", "Age.40.to.49.years" = "40 to 49",
+                                      "Age.50.to.59.years" = "50 to 59", "Age.60.years.and.over" = "Over 60"))
+    })
+    
+    
+    output$user_age_female<- renderPlot({
+        #making data frame
+        user_data1 <- read_excel_allsheets(input$user_file$datapath)[1]
+        user_data1 <- data.frame(user_data1)
+        
+        #pivot longer
+        user_data1 <- pivot_longer(user_data1, cols = Age.Under.20.years: Age.60.years.and.over, names_to = "age_group")
+        
+        #setting factors so bar chart in right order
+        user_data1$age_group <- factor(user_data1$age_group,levels = c("Age.Under.20.years", 
+                                                                       "Age.20.to.29.years", 
+                                                                       "Age.30.to.39.years", 
+                                                                       "Age.40.to.49.years",
+                                                                       "Age.50.to.59.years",
+                                                                       "Age.60.years.and.over"))
+        #plotting bar chart
+        ggplot(data = user_data1[13:18,]) + 
+            geom_col(aes(x = age_group, y = value)) +
+            labs(title = "Number of Females per Age Group for Your Company", 
+                 x = "Age Groups", y = "Total") +
+            scale_x_discrete(labels=c("Age.Under.20.years" = "Under 20", "Age.20.to.29.years" = "20 to 29",
+                                      "Age.30.to.39.years" = "30 to 39", "Age.40.to.49.years" = "40 to 49",
+                                      "Age.50.to.59.years" = "50 to 59", "Age.60.years.and.over" = "Over 60"))
+    })
+    
+    
+
+    
+    
+    
+
+    #Making a table for each user input sheet 
     output$user_age<- renderTable({
         user_data1 <- read_excel_allsheets(input$user_file$datapath)[1]
         
         if (is.null(user_data1))
-            {return(NULL)}
+        {return(NULL)}
         else
-            {return(user_data1)}
+        {return(user_data1)}
     })
-    
+
     output$user_race<- renderTable({
         user_data2 <- read_excel_allsheets(input$user_file$datapath)[2]
-        
+     
         if (is.null(user_data2))
             {return(NULL)}
         else
@@ -55,6 +167,11 @@ shinyServer(function(input, output, session) {
             {return(user_data3)}
     }) 
         
+    
+    
+    
+    
+    
     
     
     #Savannah's Output for her Page
